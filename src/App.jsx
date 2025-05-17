22fc4dc38ac862e679ff5299136176bd5f46e818
hanif-alkahfy <hanifalkahfy@gmail.com>
2025-05-17 22:56:32 +0700

    delete Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import HomeSection from './components/HomeSection';
 import Navbar from './components/Navbar';
-import { Analytics } from "@vercel/analytics/next"
 
 function App() {
   return (
     <div>
-      <Analytics />
       <Navbar />
       <HomeSection />
     </div>
