014cd498dcfbb4c92bc33d480097b124c422a1e5
hanif-alkahfy <hanifalkahfy@gmail.com>
2025-05-17 22:52:33 +0700

    add Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import HomeSection from './components/HomeSection';
 import Navbar from './components/Navbar';
+import { Analytics } from "@vercel/analytics/next"
 
 function App() {
   return (
     <div>
+      <Analytics />
       <Navbar />
       <HomeSection />
     </div>
